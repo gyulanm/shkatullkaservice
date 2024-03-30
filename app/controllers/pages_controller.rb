9ab853fe7c36e27d40ff5b2d049c1cc264734c3b
gyulanm <gyulan@mail.ru>
2024-03-30 09:46:43 +0300

    new fix

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,33 @@
+class PagesController < ApplicationController
+  def index;end
+
+  def blog;end
+
+  def promo;end
+
+  def search;end
+
+  def stones;end
+
+  def enter;end
+
+  def history;end
+
+  def jewelry;end
+
+  def jewelrystory;end
+
+  def post;end
+
+  def publish;end
+
+  def register;end
+
+  def support;end
+
+  def tiffany;end
+
+  def trends;end
+
+  def zircon;end
+end
